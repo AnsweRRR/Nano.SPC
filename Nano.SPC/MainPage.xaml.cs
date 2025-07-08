afc322823212e86468a9cc42ead8a7220b36b2dc
Tamás Pogrányi <pogranyitamas99@gmail.com>
2025-07-08 15:37:54 +0200

    Add project files.

---
+++ Nano.SPC/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Nano.SPC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Nano.SPC/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Nano.SPC
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
